304f95adfed5adcc73a0e8158fe604cd51a9072c
Rodrigo <ropenteadomc@gmail.com>
2023-02-15 14:33:00 -0300

    No comments.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from flask_gravatar import Gravatar
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = 8BYkEfBA6O6donzWlSihBXox7C0sKR6b
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
